e8d5276d63e571a4fd286536b841ef5eb2e576dd
Jaroslav Páral <paral.jarek@gmail.com>
2017-10-30 19:15:04 +0100

    Add packages doublecmd and skype

--- install_chocolatey_basic.bat
+++ install_chocolatey_basic.bat
@@ -1,3 +1,3 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install -y teamviewer firefox googlechrome flashplayerplugin jre8 javaruntime treesizefree winrar 7zip vlc totalcommander irfanview adobereader pspad notepadplusplus libreoffice pdfcreator f.lux poweriso
+choco install -y teamviewer firefox googlechrome flashplayerplugin jre8 javaruntime treesizefree winrar 7zip vlc doublecmd skype irfanview adobereader pspad notepadplusplus libreoffice pdfcreator f.lux poweriso
