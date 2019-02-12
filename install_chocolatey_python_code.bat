6dba2b30c38e4198818ccc04040396f008384060
JarekParal <paral.jarek@gmail.com>
2019-02-12 18:25:35 +0100

    Update install_chocolatey_python_code.bat

--- install_chocolatey_python_code.bat
+++ install_chocolatey_python_code.bat
@@ -1,4 +1,9 @@
-choco install -y firefox googlechrome 7zip python3 eagle visualstudiocode vlc git tortoisegit hg tortoisehg cygwin pip kicad mobaxterm altap-salamander inkscape gimp adobereader pspad notepadplusplus libreoffice pdfcreator miktex texstudio pandoc arduino clion-ide pycharm-community f.lux mobaxterm winscp poweriso autodesk-fusion360
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+choco install -y firefox googlechrome 7zip python3 visualstudiocode vlc git tortoisegit hg tortoisehg cygwin pip kicad mobaxterm inkscape gimp adobereader pspad notepadplusplus pdfcreator miktex texstudio pandoc arduino clion-ide pycharm-community f.lux winscp autodesk-fusion360 bandizip honeyview bsplayer
 pip install pygame pyserial numpy matplotlib py2exe
 code --install-extension ms-vscode.cpptools
 code --install-extension platformio.platformio-ide 
+code --install-extension streetsidesoftware.code-spell-checker
+code --install-extension ban.spellright
+code --install-extension eamodio.gitlens
