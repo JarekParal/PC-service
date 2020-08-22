f9c23f81a0187b08d0eb0d6623fc945f116cdde9
Jaroslav Páral <paral.jarek@gmail.com>
2020-08-22 03:33:10 +0200

    Update install_chocolatey_python_code.bat

--- install_chocolatey_python_code.bat
+++ install_chocolatey_python_code.bat
@@ -1,6 +1,6 @@
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-choco install -y firefox googlechrome 7zip python3 visualstudiocode vlc git tortoisegit hg tortoisehg cygwin pip kicad mobaxterm inkscape gimp adobereader pspad notepadplusplus pdfcreator miktex texstudio pandoc arduino clion-ide pycharm-community f.lux winscp autodesk-fusion360 bandizip honeyview bsplayer
+choco install -y firefox googlechrome 7zip python3 visualstudiocode vlc git tortoisegit tortoisesvn pip kicad mobaxterm inkscape gimp adobereader pspad notepadplusplus pdfcreator miktex texstudio pandoc arduino winscp autodesk-fusion360 bandizip honeyview bsplayer
 pip install pygame pyserial numpy matplotlib py2exe
 code --install-extension ms-vscode.cpptools
 code --install-extension platformio.platformio-ide 
